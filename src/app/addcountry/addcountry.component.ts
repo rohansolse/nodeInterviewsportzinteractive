e370cbf12d1d1559644565fe45af23fabc68d8fe
Rohan Solse <rohan.solse@vernost.in>
2021-04-26 01:10:12 +0530

    bugs resolved

--- src/app/addcountry/addcountry.component.ts
+++ src/app/addcountry/addcountry.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { CountriesServiceService } from '../services/countries-service.service';
 
 @Component({
@@ -11,16 +12,19 @@ export class AddcountryComponent implements OnInit {
     registerForm: FormGroup;
     submitted = false;
     image: any = null;
+    imageError: string = null;
     imagePath: string = null;
-    rankExisted: boolean = false
-    imageName: string = "Choose file"
+    rankExisted: string = null;
+    successMsg: boolean = false
+    imageName: string = "Choose File"
 
     constructor(private formBuilder: FormBuilder,
-        private apiService: CountriesServiceService) { }
+        private apiService: CountriesServiceService,
+        private router: Router) { }
 
     ngOnInit() {
         this.registerForm = this.formBuilder.group({
-            name: ['', [Validators.required, Validators.pattern("^[a-zA-Z]+$"), Validators.minLength(3), Validators.maxLength(20)]],
+            name: ['', [Validators.required, Validators.pattern("^[a-zA-Z ]*$"), Validators.minLength(3), Validators.maxLength(20)]],
             rank: ['', Validators.required],
             image: ['', [Validators.required]]
         });
@@ -38,20 +42,28 @@ export class AddcountryComponent implements OnInit {
             // console.log(response);
             if (response['status']) {
                 this.imagePath = response['data']['filePath']
+                this.imageError = null
+            }
+            else if (!response['status']) {
+                this.imageError = response['message']
             }
         }
     }
 
     async checkImamgeRank(event) {
-        console.log(event.target.value);
-        if (event.target.value && event.target.value != "") {
-            let response = await this.apiService.checkExistingRank(event.target.value)
-            // console.log(response);
-            this.rankExisted = response['status']
+        let rank = Number(event.target.value)
+        // console.log(rank);
+        if (rank > 0) {
+            let response = await this.apiService.checkExistingRank(rank)
+            if (response['status']) {
+                this.rankExisted = "This Rank is Already Existed"
+            }
+            else { this.rankExisted = null }
         }
-        else {
-            this.rankExisted = !this.rankExisted
+        else if (rank < 0) {
+            this.rankExisted = "Only Positive numbers allowed!"
         }
+        else { this.rankExisted = null }
     }
 
     async onSubmit() {
@@ -62,11 +74,16 @@ export class AddcountryComponent implements OnInit {
             rank: this.registerForm.value.rank,
             flag: this.imagePath
         }
-        let response = await this.apiService.saveCountry(saveObj)
-        if (response['status']) {
-            this.submitted = false
-            this.imageName = "Choose flag"
-            this.registerForm.reset()
+        if (this.registerForm.valid && !this.rankExisted && !this.imageError) {
+            let response = await this.apiService.saveCountry(saveObj)
+            if (response['status']) {
+                this.submitted = false
+                this.imageName = "Choose File"
+                this.registerForm.reset()
+                this.successMsg = true
+                // I have set this timeout coz after saving file app is taking time to restart
+                setTimeout(() => { this.router.navigate(["/countries"]) }, 1000);
+            }
         }
     }
 }
